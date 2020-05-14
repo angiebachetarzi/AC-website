14dd0aafe56d838dfe3571a0e77113695e1c04d6
bachetaa <angie.bachetarzi@grenoble-inp.org>
2020-05-14 13:30:49 +0200

    Set up server

--- server.js
+++ server.js
@@ -1,48 +1,50 @@
+//Définition des modules
 const express = require("express");
-var createError = require('http-errors');
-const mongoose = require("mongoose"); 
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+//Connexion à la base de donnée
+mongoose
+  .connect("mongodb://localhost/db")
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((e) => {
+    console.log("Error while DB connecting");
+    console.log(e);
+  });
 
 //On définit notre objet express nommé app
 const app = express();
 
 //Body Parser
 const urlencodedParser = bodyParser.urlencoded({
-    extended: false
+  extended: true
 });
 app.use(urlencodedParser);
+
 app.use(bodyParser.json());
 
-//Using cors CORS
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-// catch 404 and forward to error handler
+//Définition des CORS
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-
-//On définit la route Hello
-app.get('/hello',function(req,res){
-    res.json("Hello World")
-})
+//Définition du routeur
+const router = express.Router();
+app.use("/user", router);
+require(__dirname + "/controllers/userController")(router);
 
 //Définition et mise en place du port d'écoute
-const port = 9000;
+const port = 8800;
 app.listen(port, () => console.log(`Listening on port ${port}`));
