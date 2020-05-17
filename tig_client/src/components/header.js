fe649a6c821565a09460b3f53518cd4e215f2879
bachetaa <angie.bachetarzi@grenoble-inp.org>
2020-05-17 12:37:17 +0200

    log off btn works

--- tig_client/src/components/header.js
+++ tig_client/src/components/header.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import API from "../utils/API";
 import '../styles/header.css';
 
 
 class Header extends React.Component {
+
+  disconnect = () => {
+    API.logout();
+    window.location = "/";
+  };
 
   render() {
     return (
@@ -13,7 +19,7 @@ class Header extends React.Component {
         <button className="btn navbar-nav-link">Designs</button>
         <button className="btn navbar-nav-link" >Critters</button>
         <button className="btn navbar-nav-link">Turnip</button>
-        <button className="btn navbar-nav-link" id="logoff">Log off</button>
+        <button className="btn navbar-nav-link" onClick={this.disconnect} id="logoff">Log off</button>
       </nav>
        
       </div>
